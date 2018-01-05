392661e664aa6d36f6685926c486f3e9dc8aa632
alex-stout <stout.alex.11@gmail.com>
2018-01-05 13:13:03 -0600

    created skeleton

---
+++ server.js
@@ -0,0 +1,34 @@
+// require all the dependencies
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+// create an instance of express
+const app = express();
+
+// configure app to use bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// ========= API ROUTES ======= //
+var apiRouter = express.Router();
+apiRouter.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the api!' });
+});
+
+
+app.use('/', apiRouter);
+// added 404 error
+app.use(function(req, res) {
+    res.status(400);
+    res.json({ status: "404" });
+})
+app.use(function(error, req, res, next) {
+    res.status(500);
+    res.json({ status: "500" });
+})
+
+app.listen(port);
+console.log('Listening on port ' + port);
