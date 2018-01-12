1b61d241452ed8dec4dbb737dd60bde94ca16bf4
alex-stout <stout.alex.11@gmail.com>
2018-01-12 10:37:19 -0600

    imporved monitoring reliability

--- monitor.js
+++ monitor.js
@@ -2,20 +2,38 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const getHrefs = require('get-hrefs');
 
-const options = {
+// options for the request to the city home page
+const cityHomepage = {
     uri: `http://www.ci.eau-claire.wi.us/`,
-    //uri: `http://www.uwec.edu/`,
     transform: function(body) {
         return cheerio.load(body);
     }
 };
 
-rp(options)
+// makes the request to the homepage
+rp(cityHomepage)
     .then(($) => {
+        // check if there is an alert banner on the homepage
         if ($('.important_alert_wrapper').html() != null) {
-            console.log("Alt Parking in effect");
+            // grab the link to in the alert banner
             var announcementLink = getHrefs($('p').has('a').html());
-            console.log(announcementLink[0]);
+            // make a request to the page of the announcement
+            rp({
+                uri: announcementLink[0],
+                transform: function(body) {
+                    return cheerio.load(body);
+                }
+            })
+                .then(($) => {
+                    // if the announcement title contains "alternate side parking"
+                    // and "is in effect", then the status will be changed
+                    if ($('.detail-title').text().includes("Alternate Side Parking in Effect")) {
+                        console.log("Alternate side parking is in effect");
+                        var announcmentInfo = $('.detail-content').text();
+                        console.log(announcmentInfo);
+                    }
+
+                })
         } else {
             console.log("Alt parking not in effect");
         }
