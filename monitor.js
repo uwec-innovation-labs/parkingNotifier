c9d6e19885fa88e19408b46c9e7bc94ef7cc4e45
Alex Stout <stoutab4384@uwec.edu>
2018-09-19 15:45:48 -0500

    getting html from city of ec page is working

--- monitor.js
+++ monitor.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 axios.get('http://www.ci.eau-claire.wi.us/')
     .then((response) => {
         if(response.status === 200) {
-        const html = response.data;
-            const $ = cheerio.load(html); 
+        const html = response.data
+            const $ = cheerio.load(html);
             console.log($.text())
     }
-    }, (error) => console.log(err) );
+    }, (error) => console.log(err) )
