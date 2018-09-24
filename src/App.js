fd6dee339237ece894a12dc44286ac660e153647
Steven Nelson <Steven.Nelson96@gmail.com>
2018-09-24 01:15:08 -0500

    Added a basic bootstrap form to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import studentSenateLogo from './studentsenate_logo.jpg';
+import InputMask from 'react-input-mask';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        name: "",
+        phoneNumber: ""
+      };
+    }
+
+    validateForm() {
+      return this.state.name.length > 0 && this.state.phoneNumber.length > 0;
+    }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+     event.preventDefault();
+   }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+
+      <div className="Signup">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlID="name" bsSize="large">
+            <ControlLabel> Name </ControlLabel>
+            <FormControl
+              autoFocus
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup controlID="phoneNumber" bsSize="large">
+            <ControlLabel>Phone Number</ControlLabel>
+            <FormControl
+              value={this.state.phoneNumber}
+              onChange={this.handleChange}
+              type="text"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize='medium'
+            disabled={!this.validateForm()}
+            type='submit'
+          > Sign Up
+          </Button>
+          </form>
+          </div>
     );
   }
 }
