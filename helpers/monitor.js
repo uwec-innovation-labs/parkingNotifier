73440490487edd56e0d3e4fd279c2eb239c61c17
Taylor Misch <taylormisch@gmail.com>
2018-09-25 14:26:32 -0500

    added verifcation that the top-nav string contains Contact Us

--- helpers/monitor.js
+++ helpers/monitor.js
@@ -3,13 +3,16 @@ const cheerio = require("cheerio");
 
 axios.get("http://www.ci.eau-claire.wi.us/").then(
   response => {
+    var success = false;
     if (response.status === 200) {
       const html = response.data;
       const $ = cheerio.load(html);
-      const body = $("#top_nav");
-      const bodyText = body.text();
-      console.log(bodyText);
-      //console.log($("#home_link_shield"));
+      const topNav = $("#top_nav");
+      const topNavText = topNav.text();
+      if (topNavText.includes("Contact Us")) {
+        success = true;
+        console.log("TRUE");
+      }
     }
   },
   error => console.log(err)
