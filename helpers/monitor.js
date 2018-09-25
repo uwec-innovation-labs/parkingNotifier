608e85bb7245143b7b5d5a4faed27ea78b366f33
Taylor Misch <taylormisch@gmail.com>
2018-09-25 13:12:19 -0500

    Scraped top nav bar from city site

--- helpers/monitor.js
+++ helpers/monitor.js
@@ -1,11 +1,16 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-axios.get('http://www.ci.eau-claire.wi.us/')
-    .then((response) => {
-        if (response.status === 200) {
-            const html = response.data
-            const $ = cheerio.load(html)
-            console.log($("#home_link_shield"))
-        }
-    }, (error) => console.log(err))
+axios.get("http://www.ci.eau-claire.wi.us/").then(
+  response => {
+    if (response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const body = $("#top_nav");
+      const bodyText = body.text();
+      console.log(bodyText);
+      //console.log($("#home_link_shield"));
+    }
+  },
+  error => console.log(err)
+);
