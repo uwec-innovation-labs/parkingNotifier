def801af7a2bcddb7b915dd5ed13efc6e3247a37
Steven Nelson <Steven.Nelson96@gmail.com>
2018-09-26 14:05:54 -0500

    Put each input fields in separate div, and first and last name in same div for alignment

--- src/App.js
+++ src/App.js
@@ -31,51 +31,36 @@ class App extends Component {
 
   render() {
     return (
-    <div className="App">
+<div className="App">
     <div className="content">
     <img src={require('./studentsenate_logo.png')} className="img-responsive" id="StudentSenateLogo" alt="StudentSenateLogo"/>
     </div>
-      <div className="container">
+    <div className="container">
         <h3> UWEC Parking Notifier Sign-up </h3>
         <p> During a snow emergency, the City of Eau Claire
         will activate alternate side parking rules. Complete
         the form below to sign up for text alerts when alternate
         side parking is in effect. </p>
+
         <form onSubmit={this.handleSubmit}>
-          <FormGroup controlid="name" bsSize="large">
-            <ControlLabel> Name </ControlLabel>
-            <FormControl
-              id="name"
-              autoFocus
-              type="text"
-              onChange={this.handleChange}
-              />
-            </FormGroup>
-            <FormGroup controlid="phoneNumber" bsSize="large">
-            <ControlLabel>Phone Number</ControlLabel>
-            <FormControl
-              id="phoneNumber"
-              onChange={this.handleChange}
-              type="tel"
-            />
-          </FormGroup>
-          <FormGroup controlid="email" bsSize="large">
-            <ControlLabel>UWEC Email</ControlLabel>
-            <FormControl
-              id="email"
-              onChange={this.handleChange}
-              type="email"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize='large'
-            disabled={!this.validateForm()}
-            type='submit'
-          > Sign Up
-          </Button>
-          </form>
-          </div>
+        <label for="name"> Name </label>
+    <div className="form-group" id="firstAndLastName">
+
+		  <input type="name" class="form-control" autoFocus id="firstName" placeHolder="First" onChange={this.handleChange}/>
+		  <input type="name" class="form-control" autoFocus id="lastName" placeHolder="Last" onChange={this.handleChange}/>
+		</div>
+		<div class="form-group">
+			<label for="phoneNumber"> Phone Number </label>
+			<input type="tel" class="form-control" id="phoneNumber"/>
+		</div>
+		<div class="form-group">
+			<label for="email"> UWEC Email </label>
+			<input type="email" class="form-control" id="email"/>
+    </div>
+      <Button block type="submit" bsSize="large" disabled={!this.validateForm()}> Submit </Button>
+
+    </form>
+    </div>
 
           <footer>
 
