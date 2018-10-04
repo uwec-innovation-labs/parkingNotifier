275385b8848d3aad3587d20447b3db00bd9f3b23
Taylor Misch <taylormisch@gmail.com>
2018-10-04 14:42:58 -0500

    comments in the monitor file

--- helpers/monitor.js
+++ helpers/monitor.js
@@ -23,10 +23,11 @@ axios.get("http://www.ci.eau-claire.wi.us/").then(
     var success = false;
     if (response.status === 200) {
       const html = response.data;
-      const $ = cheerio.load(html);
-      var topNav = $("#top_nav");
+      const $ = cheerio.load(html); // loads html into parsable format
+      var topNav = $("#top_nav"); //searches for the 'top_nav' id in the html
       var topNavText = topNav.text();
       if (topNavText.includes("Contact Us")) {
+        // checks for existing top nav
         success = true;
         console.log("Webscrape of Eau Claire Website Result: True");
       } else {
