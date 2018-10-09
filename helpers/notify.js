70c030c1459d7afe456a124cd620273db74ae367
Katherine Reiter <ubuntu@ip-172-31-22-150.us-west-2.compute.internal>
2018-10-09 17:35:16 +0000

    moved Twilio account details to .env

--- helpers/notify.js
+++ helpers/notify.js
@@ -1,18 +1,17 @@
-const accountSid = 'AC90f10960b6c8da620e946f9e8b4673e9';
-const authToken = '6270ea173049bcc3417dfaf10c319e69';
-const client = require('twilio')(accountSid, authToken);
+const dotenv = require('dotenv').config();
+const twilio = require('twilio');
+const client = new twilio(process.env.TWILIO_ID, process.env.TWILIO_TOKEN);
 
 const katieNumber = '+17156122163';
 const taylorNumber = '+16083230141';
 
-var numbers = [katieNumber, taylorNumber];
+var numbers = [katieNumber];
 
 numbers.forEach((number) => {
   var message = client.messages.create({
-    body: 'Hello, World!',
-    from: '+18564315302',
+    body: process.env.TWILIO_MESSAGE,
+    from: process.env.TWILIO_FROM,
     to: number
-  })
-  .then(message => console.log(message.status))
+  }).then()
   .done();
 });
