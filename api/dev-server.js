7938f999433298119454e9d5dc51353696cf29fb
Alex Stout <stoutab4384@uwec.edu>
2018-10-11 12:00:58 -0500

    allow CORS

--- api/dev-server.js
+++ api/dev-server.js
@@ -41,15 +41,6 @@ app.use(
   })
 );
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 // create the router for the routes
 var apiRouter = express.Router();
 
--- api/server.js
+++ api/server.js
@@ -36,6 +36,16 @@ setTimeout(function() {
     });
 }, 20000);
 
+// allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // request logging
 app.use(morgan("tiny"));
 
