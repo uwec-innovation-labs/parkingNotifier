d1851afad82c6abc75ad5ed5d346bda0f9756578
Alex Stout <stoutab4384@uwec.edu>
2018-10-11 01:57:26 -0500

    add semi colons I guess

--- api/server.js
+++ api/server.js
@@ -1,19 +1,19 @@
 // require all the dependencies
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const morgan = require("morgan")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 // import environment variables from .env file
-require("dotenv").config()
+require("dotenv").config();
 
 // create an instance of express
-const app = express()
-var port = process.env.PORT || 9000
+const app = express();
+var port = process.env.PORT || 9000;
 
 // import data models
-var User = require("./models/user") // get our mongoose model
-var Status = require("./models/status") // get our mongoose model
+var User = require("./models/user"); // get our mongoose model
+var Status = require("./models/status"); // get our mongoose model
 
 // connect to the database
 mongoose
@@ -26,74 +26,75 @@ mongoose
       }
     }
   )
-  .catch(err => console.error(err))
+  .catch(err => console.error(err));
 
 // request logging
-app.use(morgan("tiny"))
+app.use(morgan("tiny"));
 
 // configure app to use bodyParser
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-)
-app.use(bodyParser.json())
+);
+app.use(bodyParser.json());
 
-var apiRouter = express.Router()
+var apiRouter = express.Router();
 
 apiRouter.get("/", function(req, res) {
-  res.status(200)
+  res.status(200);
   res.json({
     success: true,
     apiDocumentation: "https://github.com/UWEC-ITC/parkingNotifier-API"
-  })
-})
+  });
+});
 
 apiRouter.route("/status/").get(function(req, res) {
-  res.status(200)
+  var test = test;
+  res.status(200);
   res.json({
     alternateSideParking: status.alternateSideParking,
     message: status.message,
     updated: status.timestamp
-  })
-})
+  });
+});
 
 apiRouter
   .route("/users")
   // subscribe a user
   .post(function(req, res) {
     if (!req.body.name || !req.body.email || !req.body.phone) {
-      res.status(400)
+      res.status(400);
       res.send({
         success: false,
         message:
           "Please include a first and last name, UWEC email address, and phone number to create a user",
         apiDocumentation: "https://github.com/UWEC-ITC/parkingNotifier-API"
-      })
-      return
+      });
+      return;
     } else if (
       req.body.email.replace(/.*@/, "") == 0 ||
       req.body.email.replace(/.*@/, "") !== "uwec.edu"
     ) {
-      res.status(400)
-      res.send({ message: "Email must be a UWEC email" })
-      return
+      res.status(400);
+      res.send({ message: "Email must be a UWEC email" });
+      return;
     } else {
       var newUser = new User({
         name: req.body.name,
         email: req.body.email,
         phone: req.body.phone
-      })
+      });
 
       // attempt to save the user
       newUser.save(function(err) {
         if (err) {
-          return res.json({ success: false, message: err.errmsg })
+          return res.json({ success: false, message: err.errmsg });
         }
-        res.json({ success: true, message: "Successfully created new user" })
-      })
+        res.json({ success: true, message: "Successfully created new user" });
+      });
     }
-  })
+  });
 
 // unsubscribe a user
 apiRouter.route("/users/:email").delete(function(req, res) {
@@ -102,18 +103,18 @@ apiRouter.route("/users/:email").delete(function(req, res) {
     if (count > 0) {
       // remove the user that matches the email number
       User.remove({ email: req.params.email }, function(err, bear) {
-        if (err) res.send(err)
-        res.json({ success: true, message: "Successfully unsubscribed" })
-      })
+        if (err) res.send(err);
+        res.json({ success: true, message: "Successfully unsubscribed" });
+      });
     } else {
-      res.status(400)
+      res.status(400);
       res.json({
         success: false,
         message: "User with that email does not exist"
-      })
+      });
     }
-  })
-})
+  });
+});
 
 apiRouter.route("/users/:phone").delete(function(req, res) {
   User.count({ phone: req.params.phone }, function(err, count) {
@@ -121,42 +122,42 @@ apiRouter.route("/users/:phone").delete(function(req, res) {
     if (count > 0) {
       // remove the user that matches the phone number
       User.remove({ phone: req.params.phone }, function(err, bear) {
-        if (err) res.send(err)
-        res.json({ success: true, message: "Successfully unsubscribed" })
-      })
+        if (err) res.send(err);
+        res.json({ success: true, message: "Successfully unsubscribed" });
+      });
     } else {
-      res.status(400)
+      res.status(400);
       res.json({
         success: false,
         message: "User with that phone number does not exist"
-      })
+      });
     }
-  })
-})
+  });
+});
 
-app.use("/", apiRouter)
+app.use("/", apiRouter);
 
 /***** ERROR PAGES *****/
 app.use(function(req, res) {
-  res.status(404)
+  res.status(404);
   res.json({
     status: "failed",
     apiDocumentation: "https://github.com/UWEC-ITC/parkingNotifier-API"
-  })
-})
+  });
+});
 
 app.use(function(error, req, res, next) {
-  res.status(500)
-  console.log(error)
+  res.status(500);
+  console.log(error);
   res.json({
     status: "failed",
     apiDocumentation: "https://github.com/UWEC-ITC/parkingNotifier-API"
-  })
-})
+  });
+});
 
 app.listen(port, function() {
-  console.log("API listening on port ", port)
-})
+  console.log("API listening on port ", port);
+});
 
 // exporting the app module
-module.exports = app
+module.exports = app;
