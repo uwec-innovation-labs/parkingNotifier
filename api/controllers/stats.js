400f57b2d8e8122b9b53e8a60f1093cbacdb8ae5
Alex Stout <stoutab4384@uwec.edu>
2018-10-16 10:03:41 -0500

    remove stat import

--- api/controllers/stats.js
+++ api/controllers/stats.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 User = mongoose.model("User");
 
 exports.countUsers = (req, res) => {
-  User.count({}, (err, count) => {
+  User.countDocuments({}, (err, count) => {
     res.status(200);
     res.json({
       count: count
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,8 @@
 module.exports = app => {
   var userController = require("../controllers/users");
-  var statController = require("../controllers/stats");
 
   app
     .route("/users")
     .get(userController.listUsers)
     .post(userController.addUser);
-
-  app.route("/stats").get();
 };
