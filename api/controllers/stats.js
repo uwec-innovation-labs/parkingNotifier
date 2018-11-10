1187ab3e2dd5fb2f83079d9a479de609f6478dda
Sarah <sarahericson33@gmail.com>
2018-11-10 15:33:35 -0600

    GET /stats
    The controller is done. I am stuck on getting more than one thing to return with the .get call...

--- api/controllers/stats.js
+++ api/controllers/stats.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var User = require("../models/user");
+var Analytics = require("../models/analytics");
 
 mongoose.model("User");
 
@@ -8,6 +9,28 @@ exports.countUsers = (req, res) => {
     res.status(200);
     res.json({
       count: count
+    });
+  });
+};
+
+mongoose.model("Analytics");
+
+exports.countMessagesSent = (req, res) => {
+  Analytics.find({}, (err, messagesSent) => {
+    if (err) return console.error(err);
+    res.status(200);
+    res.send({
+      messagesSent
+    });
+  });
+};
+
+exports.countChecksPerDay = (req, res) => {
+  Analytics.find({}, (err, webScrapesPerDay) => {
+  	if (err) return console.error(err);
+    res.status(200);
+    res.send({
+      webScrapesPerDay
     });
   });
 };
--- api/routes/stats.js
+++ api/routes/stats.js
@@ -1,5 +1,9 @@
 module.exports = app => {
   var statController = require("../controllers/stats");
 
-  app.route("/stats").get(statController.countUsers);
+  app
+  	.route("/stats")
+  	.get([statController.countUsers, statController.countMessagesSent, statController.countChecksPerDay]);
+  	//.get(statController.countMessagesSent)
+  	//.get(statController.countChecksPerDay)
 };
