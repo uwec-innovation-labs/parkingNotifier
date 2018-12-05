117ef56b24c42f51f8f0265dce12c1284bc9de98
Alex Stout <stoutab4384@uwec.edu>
2018-12-04 22:50:34 -0600

    add about us

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -15,49 +15,15 @@ class About extends Component {
           />
         </div>
         <div className="container">
-          <h1> Parking Notifier </h1>
-          <h4>The Alternate Side Parking Messaging Service</h4>
+          <h4>
+            <em>"Improving students' lives through innovative technology"</em>
+          </h4>
           <p>
-            The city of Eau Claire, WI receives, on average, 46" of snowfall per
-            year. Naturally, snow emergencies are declared many times each
-            winter. Alternate side parking is critical in helping snowplows
-            clear residential roads on time.
+            Clearwater Labs is a 100% student run software incubator with the
+            goal to provide quality products and high impact experiences. The
+            mission of the organization is "Improving students'​ lives through
+            innovative technology"​
           </p>
-          <p>
-            In neighborhoods heavily populated by students, it's very common for
-            students to receive tickets for not following the policy and
-            compliance to be very low. Low compliance of the parking rules leads
-            to decreased effectiveness of snowplows, and, ultimately, the
-            degradation of road conditions throughout winter. The goal of this
-            project is to decrease the number of tickets issued to students and
-            increase the effectiveness of residential plowing.
-          </p>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Alternate Side Parking Policy</h5>
-              <h6 className="card-subtitle mb-2 text-muted">City Code 10.38</h6>
-              <p className="card-text">
-                Whenever in the opinion of the director of community services or
-                that person's designee a snow event is declared to permit a full
-                residential plow operation then for a period of seventy-two (72)
-                hours, alternate side parking regulations shall be in effect on
-                all streets in the city of Eau Claire. Such regulations shall
-                apply as follows:
-              </p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                On even-numbered days, park on the even side of the street
-              </li>
-              <li className="list-group-item">
-                On odd-numbered days, park on the odd side of the street
-              </li>
-              <li className="list-group-item">Lasts for 72 hours</li>
-              <li className="list-group-item">
-                In effect from midnight to 5:00pm during the 72 hours
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     );
